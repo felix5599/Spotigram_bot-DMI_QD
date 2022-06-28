7e99a0fc24379a2979c87ad32355b11bf0803117
Angelo Franciamore <angelo99franciamore@gmail.com>
2022-06-28 21:58:17 +0200

    fix!: Fixed the search album function
    refact: Some refactoring in the searching functions, now there are the "album_id" and "artist_id" album
    chore: Fixed the start/info/help function

--- Bot.py
+++ Bot.py
@@ -1,30 +1,26 @@
 #!/usr/bin/python
 #Import dei vari packages
-from ast import If
-from lib2to3.pytree import _Results
 from logging import exception
 from unittest import result
 import telebot
-#from telegram import MessageId
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-#SPOTIPY SETTINGS -  Queste variabili vengono prese da Spotify for Developer
+# Access Token for Telegram Bot and Spotify for developer
 cid = "45727c4bbd944c94ad186a28bd89ab64"
 cid_secret = "682e567ecf20491a8c18a63b8dfeb94d"
+API_TOKEN = '5107605922:AAGnSznHtUHLW9nlSrLq90NqYUfgni37qGs'
 
+# Spotipy Setting
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id= cid, client_secret= cid_secret))
 
-#TOKEN Bot telegram
-API_TOKEN = '5107605922:AAGnSznHtUHLW9nlSrLq90NqYUfgni37qGs'
+# Bot Telegram
 bot = telebot.TeleBot(API_TOKEN)
 
-
-@bot.message_handler(commands=['help','info','aiuto','ciao'])
+@bot.message_handler(commands=['start','help','info','aiuto','ciao'])
 def send_welcome (message):
-    bot.reply_to(message,"""Ciao! sono SpotyBot, il mio compito è quello di aiutarti nel mondo della musica!""")
-    bot.send_message(message.chat.id, "Scrivi 'artista' per sapere le canzoni più famose del tuo artista preferito!")
-    bot.send_message(message.chat.id, "Scrivi 'album' per sapere le canzoni dell'album che cerchi!")
+    bot.reply_to(message,"Ciao! sono SpotyBot, il mio compito è quello di aiutarti nel mondo della musica!")
+    bot.send_message(message.chat.id, "Scrivi 'artista' per sapere le canzoni più famose del tuo artista preferito! \nScrivi 'album' per sapere le canzoni dell'album che cerchi!")
 
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
@@ -36,12 +32,13 @@ def echo_message(message):
         bot.register_next_step_handler(message, nome_album)
 
 
-# ========== TOP 5 TRACCIE ARTISTA ==========
+# ========== TOP 5 TRACCE ARTISTA ==========
 def nome_artista(message):
     try:
         chat_id = message.chat.id
         input_text = message.text
-        results = sp.artist_top_tracks(search_artist_id(str (input_text)))
+        artist_id = search_artist_id(str (input_text))
+        results = sp.artist_top_tracks(artist_id)
     except Exception as e: 
         bot.send_message(chat_id, "C'è stato un errore! Prova a inserire in modo corretto il nome dell'artista :/")
 
@@ -58,31 +55,36 @@ def search_artist_id(nome_artista) -> str:
     risultato_finale = items[0]
     
     return(str(risultato_finale['uri']))
-#============FINE ARTISTA==================
+# ============ FINE ARTISTA==================
 
-#  ========== TRACCIE ALBUM ==========
-#def nome_album(message):
-#    try:
-#       chat_id = message.chat.id
-#       input_text = message.text
-#       results = sp.album_tracks(search_album_id(str (input_text)))
-#    except Exception as e:
-#       bot.send_message(chat_id, "C'è stato un errore! Prova a inserire in modo corretto il nome dell'album :/")
+# ========== TRACCE ALBUM ==========
+def nome_album(message):
+    try:
+       chat_id = message.chat.id
+       input_text = message.text
+       album_id = search_album_id(str (input_text)) # Cerca l'ID dell'album corrispondente
+       results = sp.album_tracks(album_id)
+    except Exception as e:
+       bot.send_message(chat_id, "C'è stato un errore! Prova a inserire in modo corretto il nome dell'album :/")
 
-#    buffer = ''
-#    for idx,track in enumerate(results['tracks'][:5]):
-#        buffer += str(idx + 1)+ " " + track['name'] + "\n"
+    number_tracks = int (results['total']) #Prende il numero totale di tracce presenti nell'album
+    buffer = ''
+    
+    for idx,track in enumerate(results['items'][:number_tracks]):
+        buffer += str(idx + 1)+ " " + track['name'] + "\n"
 
-#    bot.send_message(chat_id, buffer)    
+    bot.send_message(chat_id, buffer)    
 
 #Funzione che ritorna l'id album
-#def search_album_id(nome_album) -> str:
-#    items = results['album']['items']
-#    results = sp.search(nome_album, limit= 1, type="album")
-#    risultato_finale = items[0]
+def search_album_id(nome_album) -> str:
+    results = sp.search(nome_album, limit= 1, type="album")
+    items = results['albums']['items']
+    risultato_finale = items[0]
     
-#    return(str(risultato_finale['uri']))
+    return(str(risultato_finale['uri']))
+# ========== FINE TRACCE ALBUM ==========
 
+# -----====== IL CODICE DEVE ESSERE SCRITTO PRIMA DI QUESTA FUNZIONE =====-----
 bot.infinity_polling()
 
 #QUA é STATA FATTA UNA BOZZA | LASCIARLA PER OGNI EVENIENZA
