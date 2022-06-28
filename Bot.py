d2d61f56af79629da85bc9fd212c287a47c43b4e
Daniel Migliaccio <104918323+DaniMigli@users.noreply.github.com>
2022-06-28 17:37:04 +0200

    Insert the research choose
    Now you can choose if you want research an artis or album (album classes on comment)

--- Bot.py
+++ Bot.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 #Import dei vari packages
 from ast import If
+from lib2to3.pytree import _Results
+from logging import exception
 from unittest import result
 import telebot
 #from telegram import MessageId
@@ -17,42 +19,67 @@ sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id= cid, clien
 API_TOKEN = '5107605922:AAGnSznHtUHLW9nlSrLq90NqYUfgni37qGs'
 bot = telebot.TeleBot(API_TOKEN)
 
-# ========== TOP 5 TRACCIE ARTISTA ==========
+
 @bot.message_handler(commands=['help','info','aiuto','ciao'])
 def send_welcome (message):
-    #bot.reply_to(message,"""/ciao sono SpotyBot, il mio compito è quello di aiutarti nel mondo della musica!""")
-    bot.send_message(message.chat.id, "Inviami il nome di un artista e ti dirò quali sono le sue 5 canzoni più famose!")
+    bot.reply_to(message,"""Ciao! sono SpotyBot, il mio compito è quello di aiutarti nel mondo della musica!""")
+    bot.send_message(message.chat.id, "Scrivi 'artista' per sapere le canzoni più famose del tuo artista preferito!")
+    bot.send_message(message.chat.id, "Scrivi 'album' per sapere le canzoni dell'album che cerchi!")
+
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    if message.text == 'artista' :
+        bot.send_message(message.chat.id, "Scrivimi il nome di un artista e ti dirò quali sono le sue 5 canzoni più famose!")
+        bot.register_next_step_handler(message, nome_artista)
+    elif message.text == 'album' :
+        bot.send_message(message.chat.id, "Scrivimi l'album che cerchi e ti darò tutte le info! :) ")
+        bot.register_next_step_handler(message, nome_album)
 
 
-#def top_artisti(message):
-#    bot.send_message(message.chat.id, "Inviami il nome di un artista e ti dirò quali sono le sue 5 canzoni più famose!")
-#    bot.register_next_step_handler(message, nome_artista)
+# ========== TOP 5 TRACCIE ARTISTA ==========
+def nome_artista(message):
+    try:
+        chat_id = message.chat.id
+        input_text = message.text
+        results = sp.artist_top_tracks(search_artist_id(str (input_text)))
+    except Exception as e: 
+        bot.send_message(chat_id, "C'è stato un errore! Prova a inserire in modo corretto il nome dell'artista :/")
 
-#def nome_artista(message):
+    buffer = '' #Buffer da utilizzare per poi inviare il messaggio
+    for idx,track in enumerate(results['tracks'][:5]):
+        buffer += str(idx + 1)+ " " + track['name'] + "\n"
+
+    bot.send_message(chat_id, buffer)
+
+# Funzione che ritorna l'ID dell'artista
+def search_artist_id(nome_artista) -> str:
+    results = sp.search(nome_artista, limit= 1, type="artist")
+    items = results['artists']['items']
+    risultato_finale = items[0]
+    
+    return(str(risultato_finale['uri']))
+#============FINE ARTISTA==================
+
+#  ========== TRACCIE ALBUM ==========
+#def nome_album(message):
 #    try:
-#        chat_id = message.chat.id
-#        input_text = message.text
-#        results = sp.artist_top_tracks(search_artist_id(str (input_text)))
-#    except Exception as e: 
-#        bot.send_message(chat_id, "C'è stato un errore! Prova a inserire in modo corretto il nome dell'artista :/")
+#       chat_id = message.chat.id
+#       input_text = message.text
+#       results = sp.album_tracks(search_album_id(str (input_text)))
+#    except Exception as e:
+#       bot.send_message(chat_id, "C'è stato un errore! Prova a inserire in modo corretto il nome dell'album :/")
 
-#    buffer = '' #Buffer da utilizzare per poi inviare il messaggio
+#    buffer = ''
 #    for idx,track in enumerate(results['tracks'][:5]):
 #        buffer += str(idx + 1)+ " " + track['name'] + "\n"
 
-#    bot.send_message(chat_id, buffer)
+#    bot.send_message(chat_id, buffer)    
 
-#  ========== TOP TRACCIE ALBUM ==========
-#@bot.message_handler(commands=['album'])
-#def top_album(message):
-#    bot.send_message(message.chat.id, "Inviami il nome di un album e ti dirò quali sono le sue canzoni!")
-   # bot.register_next_step_handler(message, nome_artista)
-
-# ========== FUNZIONE CHE DA COME RITORNO L'ID DELL'ARTISTA ==========
-#def search_artist_id(nome_artista) -> str:
-#    results = sp.search(nome_artista, limit= 1, type="artist")
-#    items = results['artists']['items']
-#   risultato_finale = items[0]
+#Funzione che ritorna l'id album
+#def search_album_id(nome_album) -> str:
+#    items = results['album']['items']
+#    results = sp.search(nome_album, limit= 1, type="album")
+#    risultato_finale = items[0]
     
 #    return(str(risultato_finale['uri']))
 
